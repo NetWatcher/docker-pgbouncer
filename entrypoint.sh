aaa82db47a60617d7510caf49510b1318f3e4498
Kenneth Shelton <kshelton@defensative.com>
2017-10-22 12:51:20 +0000

    Update entrypoint to allow overriding by environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -e
 
-PG_LOG=/var/log/postgresql/
-PG_CONFIG=/etc/pgbouncer/pgbouncer.ini
-PG_USER=postgres
+: ${PG_LOG:="/var/log/postgresql/"}
+: ${PG_CONFIG:="/etc/pgbouncer/pgbouncer.ini"}
+: ${PG_USER:="postgres"}
 
 mkdir -p ${PG_LOG}
 chmod -R 755 ${PG_LOG}
 chown -R ${PG_USER}:${PG_USER} ${PG_LOG}
 
+mkdir -p /etc/keys/database
+cp -LR /etc/keys/_database/* /etc/keys/database/
+chown -R ${PG_USER}:${PG_USER} /etc/keys/database
+chmod -R 0600 /etc/keys/database
+
 echo "Starting pgbouncer..."
-exec pgbouncer -q -u ${PG_USER} $PG_CONFIG
+exec pgbouncer -u ${PG_USER} $PG_CONFIG
